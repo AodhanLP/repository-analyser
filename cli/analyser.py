abe23cbb1e583cc5cf963a60e4f3126d5c2ed678
Shamy <ahmed.alshamy@learningpool.com>
2023-07-17 12:40:17 +0100

    added code

--- cli/analyser.py
+++ cli/analyser.py
@@ -15,6 +15,18 @@ for repo in c.repos:
         # Check for GitHub Actions
         githubActions = c.get_gha(repo)
         
+        # Check for Dependency Management
+        dependencyManagement = c.get_dependency_management(repo)
+        
+        # Check for Integration Suite (GHA)
+        integrationSuite = c.get_gha_integration(repo)
+        
+        # Check for Concurrency Rule (GHA)
+        concurrencyRule = c.get_gha_concurrency(repo)
+        
+        # Check for Mend (GHA)
+        mend = c.get_mend_gha(repo)
+
         # Return to the root directory and delete the repo
         c.return_to_root(repo)
     except:
@@ -24,13 +36,17 @@ for repo in c.repos:
 
     # Print the repository information to the console
     try:
-        print('--------------------------------')
-        print(f'Repository: {repo}')
-        print(f'Package Manager: {packageManager}')
-        print(f'Semantic Release: {semanticRelease}')
-        print(f'GitHub Actions: {githubActions}')
-        print('--------------------------------')
+        print(f'{c.CYAN}--------------------------------{c.RESET}')
+        print(f'Repository: {c.YELLOW}{repo}{c.RESET}')
+        print(f'Package Manager: {c.RED if packageManager == "No" else c.GREEN}{packageManager}{c.RESET}')
+        print(f'Semantic Release: {c.GREEN if semanticRelease == "Yes" else c.RED}{semanticRelease}{c.RESET}')
+        print(f'GitHub Actions: {c.GREEN if githubActions == "Yes" else c.RED}{githubActions}{c.RESET}')
+        print(f'Dependency Management: {c.RED if dependencyManagement == "No" else c.GREEN}{dependencyManagement}{c.RESET}')
+        print(f'Integration Suite (GHA): {c.GREEN if integrationSuite == "Yes" else c.RED}{integrationSuite}{c.RESET}')
+        print(f'Concurrency Rule (GHA): {c.GREEN if concurrencyRule == "Yes" else c.RED}{concurrencyRule}{c.RESET}')
+        print(f'Mend (GHA): {c.GREEN if mend == "Yes" else c.RED}{mend}{c.RESET}')
+        print(f'{c.CYAN}--------------------------------{c.RESET}')
     except:
-        print(f'Failed to print to the console for {repo}.')
-        print('Exiting program.')
+        print(f'{c.RED}Failed to print to the console for {repo}.')
+        print(f'{c.RED}Exiting program.')
         exit()
--- cli/config.py
+++ cli/config.py
@@ -1,10 +1,24 @@
 import os
 import json
 import glob
+import subprocess
+import datetime
+import pytz
 
 class Config:
+    
     #
-    # List of repositories to migrate
+    # Colored output
+    #
+    
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    CYAN = '\033[96m'
+    RESET = '\033[0m'
+    
+    #
+    # List of repositories to analyze
     #
 
     repos = [
@@ -18,8 +32,13 @@ class Config:
 
     # Clone the repository and change directory
     def clone_repo(repo_name):
-        os.system(f"git clone https://github.com/HT2-Labs/{repo_name}.git")
-        os.chdir(repo_name)
+        try:
+            os.system(f"git clone https://github.com/HT2-Labs/{repo_name}.git")
+            os.chdir(repo_name)
+        except:
+            print(f'Failed to clone the repository {repo_name}.')
+            print('Exiting program.')
+            exit()
 
     # Change directory back to root and delete the cloned repository
     def return_to_root(repo_name):
@@ -29,7 +48,7 @@ class Config:
     # Check the repository for 'package-lock.json' or 'yarn.lock'
     def get_package_manager(repo_name):
         try:
-            # Check if 'package-lock.json' and 'yarn.lock' exists in the repository
+            # Check if 'package-lock.json' and 'yarn.lock' exist in the repository
             packageLockJsonExists = os.path.isfile('package-lock.json')
             yarnLockExists = os.path.isfile('yarn.lock')
 
@@ -51,7 +70,7 @@ class Config:
         try:
             packageJsonExists = os.path.isfile('package.json')
 
-            if packageJsonExists == True:
+            if packageJsonExists:
                 with open('package.json', 'r') as f:
                     data = json.load(f)
 
@@ -60,12 +79,76 @@ class Config:
                 semanticRelease = False
 
             # If semanticRelease is True, return 'Yes'. Otherwise, return 'No'
-            if semanticRelease == True:
+            if semanticRelease:
                 return 'Yes'
             else:
                 return 'No'
         except:
             print(f'Failed to check for Semantic Release for {repo_name}.')
+            print('Exiting program.')
+            exit()
+            
+    # Check the repository for Integration Suite (GHA)
+    def get_gha_integration(repo_name):
+        try:
+            ymlFiles = glob.glob('.github/workflows/*.yml')
+            yamlFiles = glob.glob('.github/workflows/*.yaml')
+            allFiles = ymlFiles + yamlFiles
+            keywords = ["test", "build", "integration"]
+            for file in allFiles:
+                with open(file, 'r') as f:
+                    if any(keyword in line for line in f for keyword in keywords):
+                        return "Yes"
+            return "No"
+        except:
+            print(f'Failed to check for Integration Suite (GHA) for {repo_name}.')
+            print('Exiting program.')
+            exit()
+
+    # Check the repository for Concurrency Rule (GHA)
+    def get_gha_concurrency(repo_name):
+        try:
+            ymlFiles = glob.glob('.github/workflows/*.yml')
+            yamlFiles = glob.glob('.github/workflows/*.yaml')
+            allFiles = ymlFiles + yamlFiles
+            for file in allFiles:
+                with open(file, 'r') as f:
+                    if 'concurrency' in f.read():
+                        return "Yes"
+            return "No"
+        except:
+            print(f'Failed to check for Concurrency Rule (GHA) for {repo_name}.')
+            print('Exiting program.')
+            exit()
+
+    # Check the repository for Mend (GHA)
+    def get_mend_gha(repo_name):
+        try:
+            ymlFiles = glob.glob('.github/workflows/*.yml')
+            yamlFiles = glob.glob('.github/workflows/*.yaml')
+            allFiles = ymlFiles + yamlFiles
+            for file in allFiles:
+                with open(file, 'r') as f:
+                    if 'mend' in f.read():
+                        return "Yes"
+            return "No"
+        except:
+            print(f'Failed to check for Mend (GHA) for {repo_name}.')
+            print('Exiting program.')
+            exit()
+
+    # Check the repository for Dependency Management
+    def get_dependency_management(repo_name):
+        try:
+            process = subprocess.run(["gh", "pr", "list"], capture_output=True, text=True)
+            output = process.stdout
+            if "renovate" in output :
+                return "Renovate"    
+            elif "dependabot" in output:
+                return "Dependabot"
+            return "No"
+        except:
+            print(f'Failed to check for Dependency Management for {repo_name}.')
             print('Exiting program.')
             exit()
 
@@ -75,10 +158,18 @@ class Config:
             ymlFiles = glob.glob('.github/workflows/*.yml')
             yamlFiles = glob.glob('.github/workflows/*.yaml')
 
-            if ymlFiles or yamlFiles:
-                return 'Yes'
-            else:
-                return 'No'
+            files = ymlFiles + yamlFiles
+
+            one_year_ago = datetime.datetime.now(pytz.utc) - datetime.timedelta(days=365)
+
+            for file in files:
+                result = subprocess.run(['git', 'log', '-1', '--format=%cd', file], capture_output=True, text=True)
+                file_date = datetime.datetime.strptime(result.stdout.strip(), '%a %b %d %H:%M:%S %Y %z').replace(tzinfo=pytz.utc)
+
+                if file_date > one_year_ago:
+                    return 'Yes'
+            return 'No'  # If no recent files are found
+
         except:
             print(f'Failed to check for GitHub Actions files for {repo_name}.')
             print('Exiting program.')
